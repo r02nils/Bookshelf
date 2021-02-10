66cc409b982a97cf7f85d1f1e2f6e9988bb3a893
r02nils <r02nils@gmail.com>
2021-02-10 10:27:25 +0100

    added model and service classes

--- src/main/java/ch/bzz/bookshelf/service/TestService.java
+++ src/main/java/ch/bzz/bookshelf/service/TestService.java
@@ -1,6 +1,5 @@
 package ch.bzz.bookshelf.service;
 
-import ch.bzz.bookshelf.data.DataHandler;
 import ch.bzz.bookshelf.model.Book;
 import ch.bzz.bookshelf.model.Publisher;
 
@@ -27,14 +26,9 @@ public class TestService {
     @Path("test")
     @Produces(MediaType.APPLICATION_JSON)
     public Response test() {
-        Map<String, Book> bookMap = DataHandler.getBookMap();
-        Map<String, Publisher> publisherMap = DataHandler.getPublisherMap();
-        Book book = DataHandler.readBook("c746a291-0ef9-4b2a-8268-392b12d636bd");
-        Publisher publisher = DataHandler.readPublisher("606cc4a5-8bfd-4e24-85c1-ecf6b9814712");
-
         return Response
                 .status(200)
-                .entity(bookMap)
+                .entity("{\"Data\":\"foobar\"}")
                 .build();
     }
 }
